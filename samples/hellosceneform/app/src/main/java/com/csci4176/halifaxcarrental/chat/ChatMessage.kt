53f47bd80fc89e1f5f1d14459ebed49ce72c9b18
moobrock <moobrock1@gmail.com>
2019-11-30 14:02:36 -0400

    Removed sceneformux folder and renamed project folder

--- samples/hellosceneform/app/src/main/java/com/csci4176/halifaxcarrental/chat/ChatMessage.kt
+++
@@ -1,5 +0,0 @@
-package com.csci4176.halifaxcarrental.chat
-
-class ChatMessage(val chat_Id: String, val text: String, val from_person: String, val to_person: String, val timestamp: Long, val image:Boolean, val format:String) {
-  constructor() : this("", "", "", "", -1, false,"")
-}
